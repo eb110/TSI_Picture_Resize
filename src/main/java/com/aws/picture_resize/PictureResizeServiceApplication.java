86992ae0a6b621d8f8173628231614832e5551a5
eb110 <wfigura@op.pl>
2021-11-11 11:12:00 +0000

    project init

---
+++ src/main/java/com/aws/picture_resize/PictureResizeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aws.picture_resize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureResizeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureResizeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/picture_resize/PictureResizeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.picture_resize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureResizeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
