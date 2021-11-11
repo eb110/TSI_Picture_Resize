f3550c0bf2e637c210b6d2f2d5b0f5f49a88a0f7
eb110 <wfigura@op.pl>
2021-11-11 11:47:59 +0000

    config init

---
+++ src/main/java/com/aws/picture_resize/config/StorageConfig.java
@@ -0,0 +1,9 @@
+package com.aws.picture_resize.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+//configuration links the class with application.properties
+@Configuration
+public class StorageConfig {
+}
