14ce684bdba99f84912ebde2c38eec0f47c78ecf
eb110 <wfigura@op.pl>
2021-11-12 09:34:43 +0000

    s3 api upload ver 1.0

--- src/main/java/com/aws/picture_resize/PictureResizeServiceApplication.java
+++ src/main/java/com/aws/picture_resize/PictureResizeServiceApplication.java
@@ -2,8 +2,10 @@ package com.aws.picture_resize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PictureResizeServiceApplication {
 
 	public static void main(String[] args) {
